f4404a9087b0b676d453b6dff23b650343219c88
FireQueen-3010 <dikshapatro807@gmail.com>
2022-06-03 17:44:04 +0530

    Added backend and blog page

---
+++ blog.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Svasthyam | Fitness Blog</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font awesome icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  <header>
+        <nav>
+            <div class="logo">SV<span>ASTH</span>YAM</div>
+            <div class="open"><i class="fas fa-bars"></i></div>
+            <ul class="links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#blog" class="active">Blogs</a></li>
+                <li><a href="index.php">Logout</a></li>
+                <div class="close"><i class="fas fa-times"></i></div>
+            </ul>
+        </nav>
+        <div class="content">
+            <h1>Fitness Blog</h1>
+            <button class="btn-get"><a href="#blog">View Blogs</a></button>
+        </div>
+    </header>
+    <!-- end of header -->
+    
+
+
+    <!-- blog -->
+    <section class = "blog" id = "blog">
+      <div class = "container">
+        <div class = "title">
+          <h2>Latest Blog</h2>
+          <p>recent blogs about art & design</p>
+        </div>
+        <div class = "blog-content">
+          <!-- item -->
+          <div class = "blog-item">
+            <div class = "blog-img">
+              <img src = "images/1.jpg" alt = "">
+            </div>
+            <div class = "blog-text">
+              <span>20 January, 2020</span>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.</p>
+              <!-- <a href = "#">Read More</a> -->
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "blog-item">
+            <div class = "blog-img">
+              <img src = "images/2.jpg" alt = "">
+            </div>
+            <div class = "blog-text">
+              <span>20 January, 2020</span>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.</p>
+              <!-- <a href = "#">Read More</a> -->
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "blog-item">
+            <div class = "blog-img">
+              <img src = "images/3.jpg" alt = "">
+            </div>
+            <div class = "blog-text">
+              <span>20 January, 2020</span>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.</p>
+              <!-- <a href = "#">Read More</a> -->
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "blog-item">
+            <div class = "blog-img">
+              <img src = "images/4.jpg" alt = "">
+            </div>
+            <div class = "blog-text">
+              <span>20 January, 2020</span>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.</p>
+              <!-- <a href = "#">Read More</a> -->
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "blog-item">
+            <div class = "blog-img">
+              <img src = "images/5.jpg" alt = "">
+            </div>
+            <div class = "blog-text">
+              <span>20 January, 2020</span>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.</p>
+              <!-- <a href = "#">Read More</a> -->
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "blog-item">
+            <div class = "blog-img">
+              <img src = "images/6.jpg" alt = "">
+              <!-- <span><i class = "far fa-heart"></i></span> -->
+            </div>
+            <div class = "blog-text">
+              <span>20 January, 2020</span>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.</p>
+              <!-- <a href = "#">Read More</a> -->
+            </div>
+          </div>
+          <!-- end of item -->
+        </div>
+      </div>
+    </section>
+    <!-- end of blog -->
+
+
+    <!-- footer -->
+    <footer>
+        &copy; Svasthyam 2022
+    </footer>
+    <!-- end of footer -->
+    
+    
+  </body>
+</html>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <!-- Awesome Fonts -->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    <title>Fitness Website</title>
+    <title>Svasthyam | Home</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -21,27 +21,28 @@
             <div class="open"><i class="fas fa-bars"></i></div>
             <ul class="links">
                 <li><a href="#" class="active">Home</a></li>
-                <li><a href="#">Our Services</a></li>
-                <li><a href="#">Blogs</a></li>
-                <li><a href="#">register</a></li>
+                <li><a href="#services">Our Services</a></li>
+                <li><a href="#blogs">Blogs</a></li>
+                <li><a href="Regform.php">Register</a></li>
+                <li><a href="Loginform.php">Login</a></li>
                 <div class="close"><i class="fas fa-times"></i></div>
             </ul>
         </nav>
         <div class="content">
-            <h1>Fitness gym</h1>
-            <button class="btn-get">get started</button>
+            <h2>A Guide to a Healthy and Fit You</h2>
+            <button class="btn-get"><a href="Regform.php">Get Started</a></button>
         </div>
     </header>
     <!-- Start Services -->
-    <section class="services">
-        <h1>our services</h1>
+    <section id="services">
+        <h1>Our Services</h1>
         <div class="container">
             <div class="box">
                 <img src="./images/1.jpg">
                 <div class="info">
                     <h3>Motivational Stories</h3>
                     <p>Everyone wishes to be successful and live a healthy lifestyle. We need motivation to do all of the work necessary to achieve our goals, and we also need motivation to work out. Which we provide to our customers in order for them to work hard whenever they feel unmotivated to work out.</p>
-                    <button class="btn">more</button>
+                    <!-- <button class="btn">more</button> -->
                 </div>
             </div>
             <div class="box">
@@ -49,7 +50,7 @@
                 <div class="info">
                     <h3>Live Body Weight Workouts</h3>
                     <p>Body-weight training, or resistance training with only your body weight, can be an effective type of strength training and a good addition to your fitness programme. Bodyweight training can be just as effective as free weights or weight machines.</p>
-                    <button class="btn">more</button>
+                    <!-- <button class="btn">more</button> -->
                 </div>
             </div>
             <div class="box">
@@ -57,7 +58,7 @@
                 <div class="info">
                     <h3>Network With Others</h3>
                     <p>Some people prefer to work out alone, while others prefer to work out in groups, whether it's through regular dance forms or weight-training with your friends in gyms or even while going for an evening walk! We assist you in networking with others based on your workout preference!</p>
-                    <button class="btn">more</button>
+                    <!-- <button class="btn">more</button> -->
                 </div>
             </div>
             <div class="box">
@@ -65,13 +66,13 @@
                 <div class="info">
                     <h3>Find Health Recipes</h3>
                     <p>When it comes to your body, you can't just change it by lifting weights or walking for miles and miles on the treadmill or sweating for hours and hours at Zumba! There should be some mechanism that occurs from within as well. As a result, we provide healthy and delicious recipes here.</p>
-                    <button class="btn">more</button>
+                    <!-- <button class="btn">more</button> -->
                 </div>
             </div>
         </div>
     </section>
     <!-- Start Blog -->
-    <section class="about">
+    <section id="blogs">
         <div class="row">
             <div class="image">
                 <img src="./images/about.jpg">
@@ -80,12 +81,12 @@
                 <h1>Fitness Blog</h1>
                 <p>In today's world, fitness has become so important because modern work has become so stressful that we don't care about our health until it is severely harmed or at risk!</p>
                 <p>So, as everything has become a doorstep delivery for us in today's world, we provide you with a fitness centre at your doorstep. We assist you in making time for your workout and in networking with others who have similar workout preferences as your workout partner! This way, you can make new friends while also improving your health!</p>
-                <button class="btn">Learn more</button>
+                <button class="btn"><a href="Regform.php">Learn more</a></button>
             </div>
         </div>
     </section>
     <!-- Start Register -->
-    <section class="register">
+    <!-- <section class="register">
         <h1>register</h1>
         <form>
             <div class="input_box">
@@ -99,7 +100,7 @@
             <textarea cols="30" rows="10" placeholder="Tell us your requirements"></textarea>
             <input type="submit" value="connect" class="btn">
         </form>
-    </section>
+    </section> -->
     <!-- Start Footer -->
     <footer>
         &copy; Svasthyam 2022
