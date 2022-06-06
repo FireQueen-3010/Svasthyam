de304bf3ed608a9568a5a6a4c7ffe7e0fb0c68a0
FireQueen-3010 <dikshapatro807@gmail.com>
2022-06-06 20:01:06 +0530

    Made major changes in registeration page and update profile page fields

--- login-system/register.php
+++ login-system/register.php
@@ -2,92 +2,94 @@
 
 include 'config.php';
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
    $name = mysqli_real_escape_string($conn, $_POST['name']);
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
    $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $height = mysqli_real_escape_string($conn, $_POST['height']);
+   $weight = mysqli_real_escape_string($conn, $_POST['weight']);
+   $age = mysqli_real_escape_string($conn, $_POST['age']);
+   $goal = mysqli_real_escape_string($conn, $_POST['goal']);
    $image = $_FILES['image']['name'];
    $image_size = $_FILES['image']['size'];
    $image_tmp_name = $_FILES['image']['tmp_name'];
-   $image_folder = 'uploaded_img/'.$image;
+   $image_folder = 'uploaded_img/' . $image;
 
    $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
 
-   if(mysqli_num_rows($select) > 0){
-      $message[] = 'user already exist'; 
-   }else{
-      if($pass != $cpass){
+   if (mysqli_num_rows($select) > 0) {
+      $message[] = 'user already exist';
+   } else {
+      if ($pass != $cpass) {
          $message[] = 'confirm password not matched!';
-      }elseif($image_size > 2000000){
+      } elseif ($image_size > 2000000) {
          $message[] = 'image size is too large!';
-      }else{
-         $insert = mysqli_query($conn, "INSERT INTO `user_form`(name, email, password, image) VALUES('$name', '$email', '$pass', '$image')") or die('query failed');
+      } else {
+         $insert = mysqli_query($conn, "INSERT INTO `user_form`(name, email, password, height, weight, age, goal, image) VALUES('$name', '$email', '$pass', '$height', '$weight', '$age', '$goal', '$image')") or die('query failed');
 
-         if($insert){
+         if ($insert) {
             move_uploaded_file($image_tmp_name, $image_folder);
             $message[] = 'registered successfully!';
             header('location:login.php');
-         }else{
+         } else {
             $message[] = 'registeration failed!';
          }
       }
    }
-
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>register</title>
+   <title>Register</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<div class="form-container">
 
-   <form action="" method="post" enctype="multipart/form-data">
-      <h3>register now</h3>
-      <?php
-      if(isset($message)){
-         foreach($message as $message){
-            echo '<div class="message">'.$message.'</div>';
+   <div class="form-container">
+
+      <form action="" method="post" enctype="multipart/form-data">
+         <h3>register now</h3>
+         <?php
+         if (isset($message)) {
+            foreach ($message as $message) {
+               echo '<div class="message">' . $message . '</div>';
+            }
          }
-      }
-      ?>
-      <input type="text" name="name" placeholder="enter username" class="box" required>
-      <input type="email" name="email" placeholder="enter email" class="box" required>
-      <input type="password" name="password" placeholder="enter password" class="box" required>
-      <input type="password" name="cpassword" placeholder="confirm password" class="box" required>
-      <input type="text" name="name" placeholder="enter Height" class="box" required>
-      <input type="text" name="name" placeholder="enter Weight" class="box" required>
-      <action="/action_page.php">
-         <label for="Date Of Birth">Date Of Birth:</label>
-         <input type="Date" id="Birthday" name="Birthday" class="box" required>
-      <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png" placeholder="upload an avtaar">
-      <label>
-            <select id="Goals" name="Goals" class="box" required>
-            <option value="" selected="Goals">Goals</option>
-            <option value="Weight Loss">Weight Loss</option>
-            <option value="Weight Gain">Weight Gain</option>
-            <option value="Stay Fit">Stay Fit</option>
-   </label>
-      <input type="submit" name="submit" value="register now" class="btn">
-      <button class="btn-get"><a href="../index.php">Go to home</a></button>
-      
-      <p>already have an account? <a href="login.php">login now</a></p>
-   </form>
+         ?>
+         <input type="text" name="name" placeholder="Enter your prefered username" class="box" required>
+         <input type="email" name="email" placeholder="Enter your email" class="box" required>
+         <input type="password" name="password" placeholder="Enter password" class="box" required>
+         <input type="password" name="cpassword" placeholder="Confirm Password" class="box" required>
+         <input type="number" name="height" placeholder="Enter your Height in cm" class="box" required>
+         <input type="number" name="weight" placeholder="Enter your Weight in kg" class="box" required>
+         <input type="number" name="age" class="box" placeholder="Enter your age" required>
+         <select name="goal" class="box" placeholder="Select your Fitness Goal" required>
+            <option value="" selected="Goals">Select Fitness Goal</option>
+            <option value="Weight Loss">Lose Weight</option>
+            <option value="Weight Gain">Gain Weight</option>
+            <option value="Stay Fit">Get Fit</option>
+            <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png" placeholder="upload an avtaar">
+            <input type="submit" name="submit" value="Register Now" class="btn">
+            <button class="btn-get"><a href="../index.php">Go to home</a></button>
 
-</div>
+            <p>Already have an account? <a href="login.php">Login now!</a></p>
+      </form>
+
+   </div>
 
 </body>
+
 </html>
--- login-system/update_profile.php
+++ login-system/update_profile.php
@@ -4,24 +4,29 @@ include 'config.php';
 session_start();
 $user_id = $_SESSION['user_id'];
 
-if(isset($_POST['update_profile'])){
+if (isset($_POST['update_profile'])) {
 
    $update_name = mysqli_real_escape_string($conn, $_POST['update_name']);
    $update_email = mysqli_real_escape_string($conn, $_POST['update_email']);
+   $update_height = mysqli_real_escape_string($conn, $_POST['update_height']);
+   $update_weight = mysqli_real_escape_string($conn, $_POST['update_weight']);
+   $update_age = mysqli_real_escape_string($conn, $_POST['update_age']);
+   $update_goal = mysqli_real_escape_string($conn, $_POST['update_goal']);
 
-   mysqli_query($conn, "UPDATE `user_form` SET name = '$update_name', email = '$update_email' WHERE id = '$user_id'") or die('query failed');
+   mysqli_query($conn, "UPDATE `user_form` SET name = '$update_name', email = '$update_email', height = '$update_height', 
+   weight = '$update_weight', age = '$update_age', goal = '$update_goal' WHERE id = '$user_id'") or die('query failed');
 
    $old_pass = $_POST['old_pass'];
    $update_pass = mysqli_real_escape_string($conn, md5($_POST['update_pass']));
    $new_pass = mysqli_real_escape_string($conn, md5($_POST['new_pass']));
    $confirm_pass = mysqli_real_escape_string($conn, md5($_POST['confirm_pass']));
 
-   if(!empty($update_pass) || !empty($new_pass) || !empty($confirm_pass)){
-      if($update_pass != $old_pass){
+   if (!empty($update_pass) || !empty($new_pass) || !empty($confirm_pass)) {
+      if ($update_pass != $old_pass) {
          $message[] = 'old password not matched!';
-      }elseif($new_pass != $confirm_pass){
+      } elseif ($new_pass != $confirm_pass) {
          $message[] = 'confirm password not matched!';
-      }else{
+      } else {
          mysqli_query($conn, "UPDATE `user_form` SET password = '$confirm_pass' WHERE id = '$user_id'") or die('query failed');
          $message[] = 'password updated successfully!';
       }
@@ -30,101 +35,102 @@ if(isset($_POST['update_profile'])){
    $update_image = $_FILES['update_image']['name'];
    $update_image_size = $_FILES['update_image']['size'];
    $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
-   $update_image_folder = 'uploaded_img/'.$update_image;
+   $update_image_folder = 'uploaded_img/' . $update_image;
 
-   if(!empty($update_image)){
-      if($update_image_size > 2000000){
+   if (!empty($update_image)) {
+      if ($update_image_size > 2000000) {
          $message[] = 'image is too large';
-      }else{
+      } else {
          $image_update_query = mysqli_query($conn, "UPDATE `user_form` SET image = '$update_image' WHERE id = '$user_id'") or die('query failed');
-         if($image_update_query){
+         if ($image_update_query) {
             move_uploaded_file($update_image_tmp_name, $update_image_folder);
          }
          $message[] = 'image updated succssfully!';
       }
    }
-
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>update profile</title>
+   <title>Edit Profile</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
-<body>
-   
-<div class="update-profile">
 
-   <?php
+<body>
+
+   <div class="update-profile">
+
+      <?php
       $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE id = '$user_id'") or die('query failed');
-      if(mysqli_num_rows($select) > 0){
+      if (mysqli_num_rows($select) > 0) {
          $fetch = mysqli_fetch_assoc($select);
       }
-   ?>
+      ?>
 
-   <form action="" method="post" enctype="multipart/form-data">
-      <?php
-         if($fetch['image'] == ''){
+      <form action="" method="post" enctype="multipart/form-data">
+         <?php
+         if ($fetch['image'] == '') {
             echo '<img src="images/default-avatar.png">';
-         }else{
-            echo '<img src="uploaded_img/'.$fetch['image'].'">';
+         } else {
+            echo '<img src="uploaded_img/' . $fetch['image'] . '">';
          }
-         if(isset($message)){
-            foreach($message as $message){
-               echo '<div class="message">'.$message.'</div>';
+         if (isset($message)) {
+            foreach ($message as $message) {
+               echo '<div class="message">' . $message . '</div>';
             }
          }
-      ?>
-      <div class="flex">
-         <div class="inputBox">
-            <span>username :</span>
-            <input type="text" name="update_name" value="<?php echo $fetch['name']; ?>" class="box">
-            <span>your email :</span>
-            <input type="email" name="update_email" value="<?php echo $fetch['email']; ?>" class="box">
-            <span>update your pic :</span>
-            <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" class="box">
-            <span>update weight :</span>
-            <input type="text" name="update_weight" value="<?php echo $fetch['name']; ?>" class="box">
-            <span>change date of birth :</span>
-            <action="/action_page.php">
-         <input type="Date" id="Birthday" name="Birthday" class="box" required>
+         ?>
+         <div class="flex">
+            <div class="inputBox">
+               <span>Username</span>
+               <input type="text" name="update_name" value="<?php echo $fetch['name']; ?>" class="box">
+               <span>Email</span>
+               <input type="email" name="update_email" value="<?php echo $fetch['email']; ?>" class="box">
+               <span>Update your avtaar</span>
+               <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" class="box">
+               <span>Update Height</span>
+               <input type="text" name="update_height" value="<?php echo $fetch['height']; ?>" class="box">
+               <span>Update Weight</span>
+               <input type="text" name="update_weight" value="<?php echo $fetch['weight']; ?>" class="box">
+               <span>Update Age</span>
+               <input type="number" name="update_age" value="<?php echo $fetch['age']; ?>" class="box" required>
+            </div>
+            <div class="inputBox">
+               <input type="hidden" name="old_pass" value="<?php echo $fetch['password']; ?>">
+               <span>Old Password</span>
+               <input type="password" name="update_pass" placeholder="enter previous password" class="box">
+               <span>New Password</span>
+               <input type="password" name="new_pass" placeholder="enter new password" class="box">
+               <span>Confirm New Password</span>
+               <input type="password" name="confirm_pass" placeholder="confirm new password" class="box">
+               <span>Update Goal</span>
+               <label>
+                  <select name="update_goal" class="box" value="<?php echo $fetch['goal']; ?>" required>
+                     <option value="" selected="Goal">Select Goal</option>
+                     <option value="Weight Loss">Lose Weight</option>
+                     <option value="Weight Gain">Gain Weight</option>
+                     <option value="Stay Fit">Get Fit</option>
+               </label>
+            </div>
          </div>
-         <div class="inputBox">
-            <input type="hidden" name="old_pass" value="<?php echo $fetch['password']; ?>">
-            <span>old password :</span>
-            <input type="password" name="update_pass" placeholder="enter previous password" class="box">
-            <span>new password :</span>
-            <input type="password" name="new_pass" placeholder="enter new password" class="box">
-            <span>confirm password :</span>
-            <input type="password" name="confirm_pass" placeholder="confirm new password" class="box">
-            <span>update height :</span>
-            <input type="text" name="update_height" value="<?php echo $fetch['name']; ?>" class="box">
-            <span>update goal :</span>
-            <label>
-            <select id="Goal" name="Goals" class="box" required>
-            <option value="" selected="Goal">Goals</option>
-            <option value="Weight Loss">Weight Loss</option>
-            <option value="Weight Gain">Weight Gain</option>
-            <option value="Stay Fit">Stay Fit</option>
-   </label>
-         </div>
-      </div>
-     
-      <input type="submit" value="update profile" name="update_profile" class="btn">
-      <a  href="home.php" class="delete-btn">go back</a>
-     
-   </form>
 
-</div>
+         <input type="submit" value="update profile" name="update_profile" class="btn">
+         <a href="home.php" class="delete-btn">go back</a>
+
+      </form>
+
+   </div>
 
 </body>
+
 </html>
